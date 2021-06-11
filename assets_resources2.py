c1041e806cfc074531774a41df6c0c131cb07af6
pevitsyn <67483853+pevitsyn@users.noreply.github.com>
2021-06-11 15:43:40 +0800

    Add files via upload

---
+++ assets_resources2.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 11:13:17 2021
+
+@author: Vadym
+"""
+
+import os
+import pandas as pd
+
+os.chdir("data")
+
+resources_file = "资源信息.csv"
+resources_df = pd.read_csv(resources_file)
+assets_file ='资产信息.csv'
+assets_df = pd.read_csv(assets_file)
+assets_df = assets_df.rename(columns={"ASSETSNAME":"NAME","ASSETID":"ID"})
+resources_df = resources_df.rename(columns={"RESOURCENAME":"NAME","RESOURCEID":"ID"})
+resources_columns = resources_df.columns.tolist()
+assets_columns = assets_df.columns.tolist()
+all_columns = resources_columns + assets_columns
+common_columns = []
+for col in all_columns:
+    if col in resources_columns and col in assets_columns and col not in common_columns:
+        common_columns.append(col)
+filtered_resources_df = resources_df.filter(common_columns, axis=1)
+filtered_assets_df = assets_df.filter(common_columns, axis=1)
+res_df = filtered_assets_df.append(filtered_resources_df)
+#res_df.drop(res_df.columns[1], axis = 1, inplace = True)
+
+res_df.to_csv("assets_resources.csv")           
+
+print(res_df)
